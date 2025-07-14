8c32104915d188e26230383f2950319a66f35f1a
Caio Granato <contato.orlandodicas@gmail.com>
2025-07-14 14:50:55 -0300

    Update schedulingService.ts

--- chatwoot-message-scheduler/services/schedulingService.ts
+++ chatwoot-message-scheduler/services/schedulingService.ts
@@ -1,3 +1,9 @@
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 import { ScheduledMessage, Contact, Conversation, Attachment } from '../types';
 
